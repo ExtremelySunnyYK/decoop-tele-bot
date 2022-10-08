35ec2dd598817b977bb12dd00dd672e2a336cd04
Chia Yong Kang <chiayongkang@hotmail.com>
2022-10-08 12:28:51 +0800

    first commit - colin

---
+++ tele_bot.py
@@ -0,0 +1,44 @@
+import os
+import time
+import requests
+import pandas as pd
+import datetime as DT
+from telegram import Bot
+from telegram import ParseMode
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+import yfinance as yf
+
+load_dotenv("keys.env")
+token = str(os.getenv("TELEGRAM_BOT"))
+chatid = int(os.getenv("CHAT_ID"))
+
+
+
+def scanner():
+    try:
+
+
+                                            bot = Bot(token=token)
+                                            bot.sendMessage(chat_id=chatid,
+                                                            text="🎁 ",
+                                                            timeout=200, disable_web_page_preview=True,
+                                                            parse_mode=ParseMode.HTML)
+
+    except Exception as e:
+        print(e)
+
+    return True
+
+
+def on_error(self, status):
+    time.sleep(200)
+    print(status.text)
+
+
+def on_error(self, status_code):
+    if status_code == 420:
+        time.sleep(60)
+        return False
+
+
